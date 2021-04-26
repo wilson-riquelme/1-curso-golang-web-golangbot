1626279022f61d44f173404d334f89868beed7bb
Wilson Riquelme <wilson.riquelme@xintec.cl>
2021-04-26 19:24:09 -0400

    2- variables

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {  
-    fmt.Println("Hello World v2")
-} 
+	var width, height int = 100, 50 //declaring multiple variables
+    var age int = 25 // variable declaration
+    fmt.Println("My age is", age)
+    age = 29 //assignment
+    fmt.Println("My age is", age)
+    age = 54 //assignment
+    fmt.Println("My new age is", age , "height  is", height, "-" , width)
+    //Variable tipo objeto
+	var (
+        name   = "naveen"
+        age2    = 29
+        height2 int
+    )
+    fmt.Println("my name is", name, ", age is", age2, "and height is", height2)
+	//Short value 
+	name2, age := "wilson", 29 //short hand declaration
+
+    fmt.Println("my name is", name2, "age is", age)
+}  
