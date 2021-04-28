9a816f9f8fbf46054ea67bd50eabcb62c79b5f98
Wilson Riquelme <wilson.riquelme@xintec.cl>
2021-04-28 08:59:18 -0400

    3- Numeros complejos

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
-import "fmt"
+import (  
+    "fmt"
+)
 
 func main() {  
-	var width, height int = 100, 50 //declaring multiple variables
-    var age int = 25 // variable declaration
-    fmt.Println("My age is", age)
-    age = 29 //assignment
-    fmt.Println("My age is", age)
-    age = 54 //assignment
-    fmt.Println("My new age is", age , "height  is", height, "-" , width)
-    //Variable tipo objeto
-	var (
-        name   = "naveen"
-        age2    = 29
-        height2 int
-    )
-    fmt.Println("my name is", name, ", age is", age2, "and height is", height2)
-	//Short value 
-	name2, age := "wilson", 29 //short hand declaration
-
-    fmt.Println("my name is", name2, "age is", age)
-}  
+    c1 := complex(5, 7)
+    c2 := 8 + 27i
+    cadd := c1 + c2
+    fmt.Println("sum:", cadd)
+    cmul := c1 * c2
+    fmt.Println("product:", cmul)
+}
